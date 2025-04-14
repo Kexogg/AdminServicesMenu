041ae86dc358d80b52e49e28abeb5e1e953124ba
Geratoptus <shura.bocharov.2004@mail.ru>
2025-04-14 14:29:09 +0500

    feat(ServicesRepository): заюзал EntityRepository

--- AdminServicesMenu/AdminServicesMenu.Core/Repositories/Services/ServicesRepository.cs
+++ AdminServicesMenu/AdminServicesMenu.Core/Repositories/Services/ServicesRepository.cs
@@ -4,35 +4,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AdminServicesMenu.Core.Repositories.Services;
 
-public class ServicesRepository(AdminServicesMenuDbContext dbContext) : IServicesRepository
+public class ServicesRepository(AdminServicesMenuDbContext dbContext) : 
+    EntityRepository<Service>(dbContext), IServicesRepository
 {
-    public IQueryable<Service> GetAll() =>
-        dbContext.Services.AsQueryable();
-
-    public Task AddAsync(Service item, CancellationToken cancellationToken = default) =>
-        dbContext.Services.AddAsync(item, cancellationToken).AsTask();
-    
-    public Task AddAllAsync(IEnumerable<Service> entities, CancellationToken cancellationToken = default) =>
-        dbContext.Services.AddRangeAsync(entities, cancellationToken);
-
-    public Task UpdateAsync(Service item, CancellationToken cancellationToken = default) =>
-        dbContext.Services
+    private readonly AdminServicesMenuDbContext _dbContext = dbContext;
+    public override Task UpdateAsync(Service item, CancellationToken cancellationToken = default) =>
+        _dbContext.Services
             .Where(s => s.Id == item.Id)
             .ExecuteUpdateAsync(calls => calls
                 .SetProperty(s => s.PromoPeriod, item.PromoPeriod), cancellationToken);
     
-    public Task DeleteAsync(Service item, CancellationToken cancellationToken = default) =>
-        dbContext.Services
+    public override Task DeleteAsync(Service item, CancellationToken cancellationToken = default) =>
+        _dbContext.Services
             .Where(p => p.Id == item.Id)
-            .ExecuteDeleteAsync(cancellationToken);
-
-    public Task DeleteAllAsync(IEnumerable<Service> items, CancellationToken cancellationToken = default) =>
-        dbContext.Services
-            .Intersect(items)
-            .ExecuteDeleteAsync(cancellationToken);
-
-    public Task DeleteAllAsync(Expression<Func<Service, bool>> predicate, CancellationToken cancellationToken = default) =>
-        dbContext.Services
-            .Where(predicate)
             .ExecuteDeleteAsync(cancellationToken);
 }
