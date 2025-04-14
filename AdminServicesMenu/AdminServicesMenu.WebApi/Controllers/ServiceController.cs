be95d7d03badb2721ecf9e17eedb74ac12851e9b
Leonid <rtdddawda2534@yandex.ru>
2025-04-14 13:42:40 +0500

    add endpoints skeleton

--- AdminServicesMenu/AdminServicesMenu.WebApi/Controllers/ServiceController.cs
+++ AdminServicesMenu/AdminServicesMenu.WebApi/Controllers/ServiceController.cs
@@ -7,6 +7,7 @@ namespace AdminServicesMenu.WebApi.Controllers;
 public class ServiceController : ControllerBase
 {
     [HttpGet]
+    [Produces("application/json", "application/xml")]
     public async Task<IActionResult> GetAll([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
     {
         var data = Enumerable.Range(1, 100).Skip((page - 1) * pageSize).Take(pageSize);
@@ -21,6 +22,7 @@ public class ServiceController : ControllerBase
     }
 
     [HttpGet("{id}")]
+    [Produces("application/json", "application/xml")]
     public async Task<IActionResult> Get(string id)
     {
         return Ok();
@@ -28,18 +30,21 @@ public class ServiceController : ControllerBase
 
 
     [HttpPost]
+    [Produces("application/json", "application/xml")]
     public async Task<IActionResult> Create([FromBody] object model)
     {
         return Ok();
     }
 
     [HttpDelete("{id}")]
+    [Produces("application/json", "application/xml")]
     public async Task<IActionResult> Delete(string id)
     {
         return Ok();
     }
 
     [HttpPut("{id}")]
+    [Produces("application/json", "application/xml")]
     public async Task<IActionResult> Update(string id, [FromBody] object model)
     {
         return Ok();
