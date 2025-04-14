793be0234c458bb8c3763fa8b79768481c97c379
Leonid <rtdddawda2534@yandex.ru>
2025-04-14 17:47:17 +0500

    Я Фрик

--- AdminServicesMenu/AdminServicesMenu.Core/Repositories/EntityRepository.cs
+++ AdminServicesMenu/AdminServicesMenu.Core/Repositories/EntityRepository.cs
@@ -12,12 +12,9 @@ public abstract class EntityRepository<TEntity>(AdminServicesMenuDbContext dbCtx
 
     public IQueryable<TEntity> GetAll()
         => dbCtx.Set<TEntity>().ToList().AsQueryable();
-
-    // public Task<TEntity?> GetById(string id)
-    //     => dbCtx.Set<TEntity>().FindAsync(id).AsTask();
-    //
-    public Task<TEntity?> GetById(string id)
-        => Task.Run(() => dbCtx.Set<TEntity>().First());
+    
+    public Task<TEntity?> GetById(string id) 
+        => dbCtx.Set<TEntity>().FindAsync(id).AsTask();
 
     public Task<TEntity> AddAsync(TEntity item, CancellationToken cancellationToken = default)
     {
