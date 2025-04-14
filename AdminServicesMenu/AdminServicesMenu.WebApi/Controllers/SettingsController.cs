dd18ea1180381b5e2b093032331ca5ef7d7e6677
kexogg <kexogg@gmail.com>
2025-04-14 12:51:09 +0500

    base controllers

---
+++ AdminServicesMenu/AdminServicesMenu.WebApi/Controllers/SettingsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminServicesMenu.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class SettingsController : ControllerBase
+{
+}
