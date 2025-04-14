abb547b1e3a4aa18d2473924d1ddf9b5d8d7d00d
Geratoptus <shura.bocharov.2004@mail.ru>
2025-04-14 14:40:56 +0500

    add(PersonalSettingsRepository): репозиторий для настроек

---
+++ AdminServicesMenu/AdminServicesMenu.Core/Repositories/PersonalSettingsRepository/PersonalSettingsRepository.cs
@@ -0,0 +1,22 @@
+using AdminServicesMenu.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminServicesMenu.Core.Repositories.PersonalSettingsRepository;
+
+public class PersonalSettingsRepository(AdminServicesMenuDbContext dbContext) : 
+    EntityRepository<PersonalSettings>(dbContext), IPersonalSettingRepository
+{
+    private readonly AdminServicesMenuDbContext _dbContext = dbContext;
+
+    public override Task UpdateAsync(PersonalSettings item, CancellationToken cancellationToken = default)
+        => _dbContext.PersonalSettings
+            .Where(settings => settings.Id == item.Id)
+            .ExecuteUpdateAsync(calls => calls
+                .SetProperty(settings => settings.Favorites, item.Favorites)
+                .SetProperty(settings => settings.ModifiedAt, item.ModifiedAt), cancellationToken);
+
+    public override Task DeleteAsync(PersonalSettings item, CancellationToken cancellationToken = default)
+        => _dbContext.PersonalSettings
+            .Where(settings => settings.Id == item.Id)
+            .ExecuteDeleteAsync(cancellationToken);
+}
