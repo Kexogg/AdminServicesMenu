c0b40350145572219dd6380593baa8bba73a6e13
kexogg <kexogg@gmail.com>
2025-04-14 14:36:37 +0500

    service repository

--- AdminServicesMenu/AdminServicesMenu.Core/Repositories/RepositoriesStartup.cs
+++ AdminServicesMenu/AdminServicesMenu.Core/Repositories/RepositoriesStartup.cs
@@ -1,4 +1,6 @@
+using AdminServicesMenu.Core.Repositories.Presets;
 using AdminServicesMenu.Core.Repositories.PromoPeriods;
+using AdminServicesMenu.Core.Repositories.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AdminServicesMenu.Core.Repositories;
@@ -8,6 +10,7 @@ public static class RepositoriesStartup
     public static IServiceCollection AddRepositories(this IServiceCollection serviceCollection)
     {
         serviceCollection.AddScoped<IPresetRepository, PresetRepository>();
+        serviceCollection.AddScoped<IServicesRepository, ServicesRepository>();
         serviceCollection.AddScoped<IPromoPeriodRepository, PromoPeriodRepository>();
 
         return serviceCollection;
