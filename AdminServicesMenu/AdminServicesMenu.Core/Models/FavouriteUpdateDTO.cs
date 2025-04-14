03270c595cae4693c14ed85f510dccf5fc78b2b2
Maxim <mbrozovskii@gmail.com>
2025-04-14 13:33:45 +0500

    Add Services

---
+++ AdminServicesMenu/AdminServicesMenu.Core/Models/FavouriteUpdateDTO.cs
@@ -0,0 +1,3 @@
+namespace AdminServicesMenu.Core.Models;
+
+public record FavouriteUpdateDTO(string Id);
---
+++ AdminServicesMenu/AdminServicesMenu.Core/Services/IPresetService.cs
@@ -0,0 +1,12 @@
+using AdminServicesMenu.Core.Models;
+
+namespace AdminServicesMenu.Core.Services;
+
+public interface IPresetService
+{
+    Task<PresetReponseDTO> GetByIdAsync(string id);
+    Task<PresetReponseDTO> CreateAsync(PresetCreateDTO preset);
+    Task<PresetUpdateDTO> UpdateAsync(PresetUpdateDTO preset);
+    Task<PresetUpdateDTO> DeleteAsync(string id);
+    Task<List<PresetUpdateDTO>> GetAllAsync();
+}
