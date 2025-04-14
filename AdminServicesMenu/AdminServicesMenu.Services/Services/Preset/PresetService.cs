e9bf347881539cbc953d31a6b9d2af3f35d5dac5
kexogg <kexogg@gmail.com>
2025-04-14 17:00:48 +0500

    fixes

--- AdminServicesMenu/AdminServicesMenu.Services/Services/Preset/PresetService.cs
+++ AdminServicesMenu/AdminServicesMenu.Services/Services/Preset/PresetService.cs
@@ -13,26 +13,27 @@ public class PresetService(IPresetRepository repository, IMapper mapper) : IPres
         return repository.DeleteAsync(id);
     }
 
-    public Task<PresetResponseDTO> CreateAsync(PresetCreateDTO preset)
+    public async Task<PresetResponseDTO> CreateAsync(PresetCreateDTO preset)
     {
         var model = await repository.AddAsync(mapper.Map<PresetCreateDTO, Core.Domain.Preset>(preset));
         return new PresetResponseDTO(model.Id, model.Favorites);
     }
 
-    public Task<PresetResponseDTO> UpdateAsync(string id, PresetUpdateDTO preset)
+    public async Task<PresetResponseDTO> UpdateAsync(string id, PresetUpdateDTO preset)
     {
         var model = await repository.UpdateAsync(mapper.Map<PresetUpdateDTO, Core.Domain.Preset>(preset));
         return new PresetResponseDTO(model.Id, model.Favorites);
     }
 
-    public Task<PresetResponseDTO> GetByIdAsync(string id)
+    public async Task<PresetResponseDTO> GetByIdAsync(string id)
     {
-        var model = await repository.GetByIdAsync(id);
+        var model = await repository.GetById(id);
         return new PresetResponseDTO(model.Id, model.Favorites);
     }
 
     public Task<List<PresetResponseDTO>> GetAllAsync(in int page, int pageSize)
     {
-        throw new NotImplementedException();
+        var models = repository.GetAll();
+        return Task.FromResult(models.Select(x => new PresetResponseDTO(x.Id, x.Favorites)).ToList());
     }
 }
