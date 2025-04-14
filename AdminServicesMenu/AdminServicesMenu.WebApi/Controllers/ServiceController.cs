8bb0daa4856a8c36c19fdb4427df117badb2a743
kexogg <kexogg@gmail.com>
2025-04-14 13:14:41 +0500

    controller stub

--- AdminServicesMenu/AdminServicesMenu.WebApi/Controllers/ServiceController.cs
+++ AdminServicesMenu/AdminServicesMenu.WebApi/Controllers/ServiceController.cs
@@ -6,8 +6,41 @@ namespace AdminServicesMenu.WebApi.Controllers;
 [Route("api/[controller]")]
 public class ServiceController : ControllerBase
 {
-    [HttpPost("{serviceId:long}/favorite")]
-    public async Task<ActionResult> Favorite([FromRoute] long serviceId)
+    [HttpGet]
+    public async Task<IActionResult> GetAll([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+    {
+        var data = Enumerable.Range(1, 100).Skip((page - 1) * pageSize).Take(pageSize);
+
+        return Ok(new
+        {
+            Page = page,
+            PageSize = pageSize,
+            TotalCount = 100,
+            Items = data
+        });
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(string id)
+    {
+        return Ok();
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] object model)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        return Ok();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(string id, [FromBody] object model)
     {
         return Ok();
     }
